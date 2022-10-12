e4c6add0b05bbbea5d807464b0768788e7482366
parker334 <88067431+parker334@users.noreply.github.com>
2022-10-12 15:21:51 +1000

    add getRoute and RouteList

--- src/main/java/com/translink/api/controller/SampleController.java
+++ src/main/java/com/translink/api/controller/SampleController.java
@@ -6,6 +6,9 @@ import com.translink.api.repository.TripRepository;
 import com.translink.api.repository.model.Route;
 import com.translink.api.repository.model.Stop;
 import com.translink.api.repository.model.Trip;
+
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +52,25 @@ public class SampleController {
     public Route getRouteDetail(String id) {
 //        return RouteDTO.mapFrom(routeRepository.findByRouteId(id));
         return routeRepository.findByRouteId(id);
+    }
+
+    @GetMapping("/stopRouteList")
+    public Route[] getStopRouteDetail(String sid) {
+        ArrayList<String> routeIDList = new ArrayList<>();
+        for (int i = 0; i < stopRepository.findByStopId(sid).getStopTimes().size(); i++) {
+            String temp = stopRepository.findByStopId(sid).getStopTimes().get(i).getTrip().getRoute().getRouteId();
+            if (!(routeIDList.contains(temp))) {
+                routeIDList.add(temp);
+            }
+        }
+
+        ArrayList<Route> routeList = new ArrayList<>();
+        for (String routes : routeIDList) {
+            routeList.add(routeRepository.findByRouteId(routes));
+        }
+
+        Route[] routeArr = new Route[routeList.size()];
+        routeArr = routeList.toArray(routeArr);
+        return routeArr;
     }
 }
